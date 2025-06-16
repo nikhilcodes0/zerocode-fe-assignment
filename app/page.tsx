e39cce09fb8657ca47fbac7260762c2250da258d
nikhilcodes0 <nk7488577732@gmail.com>
2025-06-16 08:25:36 +0530

    feat: project setup with initial pages (sign-in, sign-up, chat, landing)

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,64 @@
+"use client";
+
 import Image from "next/image";
+import union from "@/public/Union.png";
+import heroLogo from "@/public/hero-logo.png";
 
-export default function Home() {
+import Link from "next/link";
+
+export default function App() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <>
+      <div
+        className="relative h-[560px] w-screen"
+        style={{
+          background:
+            "linear-gradient(180deg, rgba(255, 240, 200, 1) 0%, rgba(200, 255, 242, 1) 50%, rgba(200, 222, 255, 0) 100%)",
+        }}
+      >
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          src={union}
+          alt="union"
+          width={300}
+          height={300}
           priority
+          style={{ objectFit: "contain" }}
+          className="w-full h-fit absolute top-0 left-0 animate-fade-in-up"
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+        <Image
+          src={heroLogo}
+          alt="hero-logo"
+          width={500}
+          height={100}
+          priority
+          style={{ objectFit: "contain" }}
+          className="w-[400px] mx-auto h-fit relative top-[13rem] left-0 animate-fade-in-up animate-delay-200"
+        />
+      </div>
+      <h1 className="text-center text-3xl font-semibold mt-12 font-poppins animate-fade-in-up animate-delay-400">
+        The Most Trusted And Fast Chatbot Ever
+      </h1>
+      <p className="text-center text-lg p-4 px-7 opacity-70 font-poppins animate-fade-in-up animate-delay-400">
+        ZeroCode is the most trusted and fast chatbot ever made on internet.
+      </p>
+      <div className="flex flex-row gap-8 justify-center items-center mt-9 w-full px-4 animate-fade-in-up animate-delay-600">
+        <div className="flex justify-center items-center w-full">
+          <Link
+            href="/register"
+            className="cursor-pointer bg-black text-white py-4 w-full rounded-xl font-semibold hover:bg-black hover:text-white transition-all duration-300 text-center"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Get Started
+          </Link>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="flex justify-center items-center w-full">
+          <Link
+            href="/signin"
+            className="cursor-pointer border-black border-2 text-black py-4 w-full rounded-xl font-semibold hover:bg-black hover:text-white transition-all duration-300 text-center"
+          >
+            SignIn
+          </Link>
+        </div>
+      </div>
+    </>
   );
 }
